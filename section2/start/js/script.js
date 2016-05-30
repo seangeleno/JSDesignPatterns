06b2e7d21cc693a4d75198a972be5e26b67e6562
Sean Esteva <seangeleno@gmail.com>
2016-05-30 11:16:37 -0700

    factory design pattern

--- section2/start/js/script.js
+++ section2/start/js/script.js
@@ -1,5 +1,21 @@
 (function(win, $){
 
+	var RedCircle = function(){
+		this.item = $('<div class="circle"></div>');
+	},
+			BlueCircle = function(){
+				this.item = $('<div class="circle"></div>');
+			},
+			CircleFactory = function(){
+				this.create = function(color){
+					if(color === 'blue'){
+						return new BlueCircle();
+					}else {
+						return new RedCircle();
+					}
+				}
+			}
+
 	var CircleGeneratorSingleton = (function(){
 		var instance;
 		//all properties and methods live within init
@@ -7,15 +23,17 @@
 
 				//private methods have _
 			var _aCircle = [],
-					_stage = $('.advert');
+					_stage = $('.advert'),
+					_cf = new CircleFactory();
 
 				function _position(circle, left, top){
 					circle.css('left', left);
 					circle.css('top', top);
 				}
 
-				function create (left, top){
-					var circle = $('<div class="circle"></div>');
+				function create (left, top, color){
+
+					var circle = _cf.create(color).item;
 					_position(circle,left,top);
 					return circle;
 				}
@@ -56,9 +74,19 @@
 
 			//insert logic when clicked
 			var cg = CircleGeneratorSingleton.getInstance();
-			var circle = cg.create(e.pageX-25, e.pageY-25);
+			var circle = cg.create(e.pageX-25, e.pageY-25, "red");
 
 			cg.add(circle);
+		});
+
+		$(document).keypress (function(e){
+			if(e.key == 'a'){
+				var cg = CircleGeneratorSingleton.getInstance();
+				var circle = cg.create(Math.floor(Math.random()*600),
+															 Math.floor(Math.random()*600), "blue");
+
+				cg.add(cricle);
+			}
 		});
 
 	});
