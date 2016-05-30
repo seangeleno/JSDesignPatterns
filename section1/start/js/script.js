5e5747e14cf60c2ed72d036dae4fe3059db47584
Sean Esteva <seangeleno@gmail.com>
2016-05-30 10:04:11 -0700

    module design pattern, private and public functions

--- section1/start/js/script.js
+++ section1/start/js/script.js
@@ -1,5 +1,59 @@
-//this is known as the name space desgin pattern, by making really long name
-//we avoid using same variable more than once;
+var chatModule = (function () {
+
+	var leadself = "Me: ",
+	leadcomputer = "Mac: ",
+	aSaid = ["This is a Cyber Chat"],
+	msgYes = "Yes, that's a great idea.",
+	msgNo = "No, that must be a mistake.",
+	aSassyStuff = ["Like mold on books, grow myths on history.",
+								"She moved like a poem and smiled like a sphinx.",
+								"As long as we don’t die, this is gonna be one hell of a story.",
+								"She laughed, and the desert sang.",
+								"You’ve got about as much charm as a dead slug."];
+
+
+	 function echo (msg){
+				aSaid.push("<div>" + msg + "</div>");
+
+				var aSaidlength = aSaid.length,
+				 		start = Math.max(aSaid.length - 6, 0),
+						out = "";
+
+				for (var i = start ; i < aSaidlength ; i++){
+						out += aSaid[i];
+				}
+
+				$('.advert').html(out);
+
+				$('#talk span').text(msg);
+
+			}
+			//return public functions
+	return {
+		talk: function(msg){
+			echo ( leadself + msg);
+		},
+		replyYesNo: function(){
+			var msg = Math.random()>.5? this.msgYes : this.msgNo;
+			echo( leadcomputer + msg)
+		},
+		saySassyStuff: function(){
+			var msg = aSassyStuff[Math.floor(Math.random())*aSassyStuff.length];
+			echo( leadcomputer + msg );
+
+
+
+		}
+	};
+})();
+
+$(document).ready(function(){
+	chatModule.talk("this is great");
+	chatModule.replyYesNo();
+	chatModule.saySassyStuff();
+})
+
+/*
 var com = com || {};
 com.o2geek = com.o2geek || {};
 com.o2geek.packt = com.o2geek.packt || {};
@@ -18,17 +72,6 @@ com.o2geek.packt.JSDP.sudoChat = {
 									"As long as we don’t die, this is gonna be one hell of a story.",
 									"She laughed, and the desert sang.",
 									"You’ve got about as much charm as a dead slug."],
-		talk: function(msg){
-			this.echo ( this.leadself + msg);
-		},
-		replyYesNo: function(){
-			var msg = Math.random()>.5? this.msgYes : this.msgNo;
-			this.echo(this.leadcomputer + msg)
-		},
-		saySassyStuff: function(){
-			var msg = this.aSassyStuff[Math.floor(Math.random())*aSassyStuff.length];
-			this.echo(this.leadcomputer + msg);
-		},
 		echo: function(msg){
 			this.aSaid.push("<div>" + msg + "</div>");
 
@@ -47,3 +90,4 @@ com.o2geek.packt.JSDP.sudoChat = {
 		}
 
 };
+*/
